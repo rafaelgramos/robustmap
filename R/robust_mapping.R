278fb6fc86f19df7a93eaf381c71ca05545dece0
Rafael Ramos <rafaelgramos@gmail.com>
2024-04-18 08:50:54 -0400

    updating to pull from external example.R file

--- R/robust_mapping.R
+++ R/robust_mapping.R
@@ -42,20 +42,7 @@
 #' @import terra
 #' @export
 #' 
-#' @examples
-#' library(robustmap)
-#' # Loading point data
-#' burglary <- sf::read_sf(dsn = "data", layer = "burglary")
-#' burglary <- data.frame(lon=burglary$lon_m,
-#'                        lat=burglary$lat_m)
-#' # Estimating optimal granularity using robust.quadcount
-#' burglary_map <- robust.quadcount(burglary,verbose = T)
-#' 
-#' # Retriving estimated granularity
-#' burglary_map$opt_granularity
-#' 
-#' # Plotting resulting map
-#' terra::plot(burglary_map$counts)
+#' @example examples/example.R
 #' 
 
 robust.quadcount<-function(point_set,
